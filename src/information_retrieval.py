ee5c812571f27290d4c7de7a2b4008131246310c
drumpt <ssohot1@kaist.ac.kr>
2021-05-16 12:19:29 +0900

    Fix preprocess

--- src/information_retrieval.py
+++ src/information_retrieval.py
@@ -1,1 +1,4 @@
-pass
+# TODO: LDA or WordRank
+# TODO: Tf-Idf with various formula
+# TODO: BM25
+# TODO: Identify the events related issue from the article
--- src/preprocess.py
+++ src/preprocess.py
@@ -4,44 +4,45 @@ import json
 
 import nltk
 
-def tokenization(self, sentence):
-    return nltk.tokenize.word_tokenize(sentence)
+def tokenization(body):
+    '''
+    Break body into list of words
+    '''
+    return nltk.tokenize.word_tokenize(body)
 
-def remove_punctuation(self, words):
-    return [word for word in words if not word in string.punctuation and not word[0].isdigit()]
+def remove_punctuation(words):
+    '''
+    Remove all the punctuations like "!()-[]{};:'"\,<>./?@#$%^&*_~" in words
+    '''
+    return [word for word in words if not word in string.punctuation]
 
-def identifier_normalization(self, words):
-    normalized_words = []
-    for word in words:
-        if self.snake_case_breakdown(word)[0] != word:
-            normalized_words.extend(self.snake_case_breakdown(word))
-        elif word.isalnum() and self.camel_case_breakdown(word)[0] != word:
-            normalized_words.extend(self.camel_case_breakdown(word))
-        else:
-            normalized_words.append(word)
-    return normalized_words
-
-def case_normalization(self, words):
+def case_normalization(words):
+    '''
+    'File' and 'FILE' will become 'file'
+    '''
     return [word.lower() for word in words]
 
-def stop_word_filtering(self, words):
+def stop_word_filtering(words):
+    '''
+    Some words like 'a', 'an' are extremely common in English. Filter these words.
+    '''
     stop_words = nltk.corpus.stopwords.words("english")
     return [word for word in words if not word in stop_words]
 
-def stemming(self, words):
+def stemming(words):
+    '''
+    'opens' and 'opening' both become 'open'
+    '''
     snowball = nltk.stem.SnowballStemmer("english")
     return [snowball.stem(word) for word in words]
 
-def snake_case_breakdown(self, identifier):
-    return identifier.split("_")
-
-def camel_case_breakdown(self, identifier):
-    idx = list(map(str.isupper, identifier))
-    l = [0]
-    for (i, (x, y)) in enumerate(zip(idx, idx[1:])):
-        if x and not y: # "Ul"
-            l.append(i)
-        elif not x and y: # "lU"
-            l.append(i+1)
-    l.append(len(identifier))
-    return [identifier[x:y] for x, y in zip(l, l[1:]) if x < y]
+def preprocess_body(body):
+    '''
+    tokenization -> remove_punctuation -> case_normalization -> stop_word_filtering -> stemming
+    '''
+    words = tokenization(body)
+    words = remove_punctuation(words)
+    words = case_normalization(words)
+    words = stop_word_filtering(words)
+    words = stemming(words)
+    return words
